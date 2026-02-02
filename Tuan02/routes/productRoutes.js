760f2dee548c3c431a63c2aa2ebf75c5513c1222
TuanANh561 <nguyentuananh11a1.lhpt@gmail.com>
2026-02-02 14:51:02 +0700

    refactor

--- Tuan02/routes/productRoutes.js
+++ Tuan02/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middlewares/upload');
+const upload = require('../config/upload');
 
 const productController = require('../controllers/productController')
 
